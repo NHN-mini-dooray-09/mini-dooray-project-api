4fe7287044329cc4fe9efec6a227392587026d34
jeong <jeongm0220@gmail.com>
2023-06-05 10:26:12 +0900

    docs : mini-dooray-project-api repository 생성
    mini dooray project api repository 생성

---
+++ src/main/java/com/nhnacademy/minidoorayporjectapi/MiniDoorayPorjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidoorayporjectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDoorayPorjectApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDoorayPorjectApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/minidoorayporjectapi/MiniDoorayPorjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidoorayporjectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDoorayPorjectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
